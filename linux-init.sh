bed7ce50b4cfd48113b1ec13f35a1d81d704e2a9
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2021-08-02 20:03:03 +0530

    Added alsa-utils alsa-firmware & breeze

--- linux-init.sh
+++ linux-init.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-pacman -S sudo alacritty nitrogen wmctrl dmenu wget curl xmonad xmonad-contrib xmobar qtile lxappearance qt5ct base-devel git upower ufw obs-studio vi vim neovim mupdf ranger volumeicon network-manager-applet flameshot scrot sagemath octave code bash-completion texlive-most firefox-developer-edition gimp inkscape libreoffice-fresh vlc imagemagick telegram-desktop element-desktop 
+pacman -S sudo alacritty nitrogen picom wmctrl dmenu wget curl alsa-utils alsa-firmware xmonad xmonad-contrib xmobar qtile lxappearance qt5ct breeze breeze-gtk breeze-icons base-devel git upower ufw obs-studio vi vim neovim mupdf ranger volumeicon network-manager-applet flameshot scrot sagemath octave code bash-completion texlive-most firefox-developer-edition gimp inkscape libreoffice-fresh vlc imagemagick telegram-desktop element-desktop 
 
 
 
 git clone https://aur.archlinux.org/yay-git.git
-cd yay-git
-makepkg -si
+# cd yay-git
+# makepkg -si
 
-yay -S awesome-git
+# yay -S awesome-git
