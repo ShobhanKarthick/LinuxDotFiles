6f5e42de053fa6e51ba5fe6076d1271653311bd5
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2021-08-02 20:00:46 +0530

    Added wmctrl and dmenu to installation script

--- linux-init.sh
+++ linux-init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-pacman -S sudo alacritty nitrogen wget curl xmonad xmonad-contrib xmobar qtile lxappearance qt5ct base-devel git upower ufw obs-studio vi vim neovim mupdf ranger volumeicon network-manager-applet flameshot scrot sagemath octave code bash-completion texlive-most firefox-developer-edition gimp inkscape libreoffice-fresh vlc imagemagick telegram-desktop element-desktop 
+pacman -S sudo alacritty nitrogen wmctrl dmenu wget curl xmonad xmonad-contrib xmobar qtile lxappearance qt5ct base-devel git upower ufw obs-studio vi vim neovim mupdf ranger volumeicon network-manager-applet flameshot scrot sagemath octave code bash-completion texlive-most firefox-developer-edition gimp inkscape libreoffice-fresh vlc imagemagick telegram-desktop element-desktop 
 
 
 
