bb8f6626fe55dd73beef6cb5577da098c774f547
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2021-08-03 15:07:39 +0530

    Added pavucontrol pulseaudio bluez-utils pulseaudio-alsa pulseaudio-bluetooth bluez bluez-utils

--- linux-init.sh
+++ linux-init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-pacman -S sudo alacritty nitrogen picom wmctrl dmenu wget curl alsa-utils alsa-firmware xmonad xmonad-contrib xmobar qtile lxappearance qt5ct breeze breeze-gtk breeze-icons base-devel git upower ufw obs-studio vi vim neovim mupdf ranger volumeicon network-manager-applet flameshot scrot sagemath octave code bash-completion texlive-most firefox-developer-edition gimp inkscape libreoffice-fresh vlc imagemagick telegram-desktop element-desktop 
+pacman -S sudo alacritty nitrogen picom wmctrl dmenu wget curl alsa-utils alsa-firmware pavucontrol pulseaudio pulseaudio-alsa pulseaudio-bluetooth bluez bluez-utils xmonad xmonad-contrib xmobar qtile lxappearance qt5ct breeze breeze-gtk breeze-icons base-devel git upower ufw obs-studio vi vim neovim mupdf ranger volumeicon network-manager-applet flameshot scrot sagemath octave code bash-completion texlive-most firefox-developer-edition gimp inkscape libreoffice-fresh vlc imagemagick telegram-desktop element-desktop 
 
 
 
