c59bf8788513172c2bed5d17b2cd37703dfb42f2
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2022-02-12 10:53:51 +0530

    Pulled dwmblocks from basic repo & added statuscmd patch

--- dwmblocks/blocks.def/battery.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#ICON=$'\360\237\224\213'
-ICON=$'\360\237\224\213'
-read -r capacity </sys/class/power_supply/BAT0/capacity
-printf "$ICON%s%%" "$capacity"
--- dwmblocks/blocks.def/calendar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ICON=$'\360\237\223\205'
-printf "$ICON%s" "$(date '+%a, %b %d, %R')"
