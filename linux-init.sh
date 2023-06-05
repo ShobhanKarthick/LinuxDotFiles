0a50ff8a38af7ba8992af7964d609c21a24c4471
Shobhan Karthick <karthickshobhan@gmail.com>
2023-06-06 01:46:42 +0530

    feat: added few more essential packages

--- linux-init.sh
+++ linux-init.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 
-pacman -S sudo alacritty nitrogen picom wmctrl dmenu wget curl alsa-utils alsa-firmware pavucontrol pulseaudio pulseaudio-alsa pulseaudio-bluetooth bluez bluez-utils xmonad xmonad-contrib xmobar qtile lxappearance qt5ct breeze breeze-gtk breeze-icons base-devel git upower ufw obs-studio vi vim neovim mupdf ranger volumeicon network-manager-applet flameshot scrot sagemath octave code bash-completion texlive-most firefox-developer-edition gimp inkscape libreoffice-fresh vlc imagemagick telegram-desktop element-desktop 
-
-
+pacman -S sudo alacritty nitrogen picom wmctrl dmenu wget curl alsa-utils alsa-firmware pavucontrol pulseaudio pulseaudio-alsa pulseaudio-bluetooth bluez bluez-utils xmonad xmonad-contrib xmobar qtile lxappearance qt5ct breeze breeze-gtk breeze-icons base-devel git upower ufw obs-studio vi vim neovim mupdf ranger volumeicon network-manager-applet flameshot scrot sagemath octave code bash-completion texlive-most firefox-developer-edition gimp inkscape libreoffice-fresh vlc imagemagick telegram-desktop lf notification-daemon ntfs-3g stalonetray sxhkd ttf-joypixels ttf-nerd-fonts-symbols ttf-roboto-mono-nerd wmctrl xclip xf86-input-synaptics xorg-server xorg-xinit xorg-xwayland qt5-wayland glfw-wayland kitty nitrogen
 
 git clone https://aur.archlinux.org/yay-git.git
 # cd yay-git
